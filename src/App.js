8b99faef91d38c9a964f33995d788653bafb9cda
Sumeru <sumerudas2019@gmail.com>
2023-07-21 14:27:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen.js';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import LoginScreen from './screens/LoginScreen';
+import LoginScreen from './screens/LoginScreen.js';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './features/userSlice';
 
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, userAuth => {
+      if (userAuth) {
+          dispatch(
+            login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+        } else {
+            dispatch(logout);
+        }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
         <Router> 
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-import firebase from './firebase';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAM7y7WboWRnMz4HFZMUfX0ndeCD3BaCOk",
-    authDomain: "netflix-build-8d899.firebaseapp.com",
-    projectId: "netflix-build-8d899",
-    storageBucket: "netflix-build-8d899.appspot.com",
-    messagingSenderId: "826816842435",
-    appId: "1:826816842435:web:d9ad8545aa90c88f56f076",
-    measurementId: "G-022TFLTQGK"
-  };
+  apiKey: "AIzaSyAM7y7WboWRnMz4HFZMUfX0ndeCD3BaCOk",
+  authDomain: "netflix-build-8d899.firebaseapp.com",
+  projectId: "netflix-build-8d899",
+  storageBucket: "netflix-build-8d899.appspot.com",
+  messagingSenderId: "826816842435",
+  appId: "1:826816842435:web:d9ad8545aa90c88f56f076",
+  measurementId: "G-022TFLTQGK"
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  const auth = getAuth(firebaseApp);
 
   export { auth };
   export default db;
+
